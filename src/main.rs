c380a424d7a9de524351ff726562846fcdd5b3f5
kumasan903 <kawanishi.souta.20050718@gmail.com>
2023-04-26 12:19:27 +0900

    クラッシュする問題を解決

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ fn main() {
     let mut src_path;
 	let mut dst_path1;
 	let mut dst_path2;
-	let sleep_time;
+	let mut try_count;
+	let sleep_time = time::Duration::from_millis(500);
 
 	println!("whazzup_helper is running...");
-	sleep_time = time::Duration::from_millis(500);
+	try_count = 0;
 	src_path = dirs::home_dir().expect("ホームディレクトリの取得に失敗しました");
 	src_path.push("Appdata/Local/JoinFS-FS2020/whazzup.txt");
 	dst_path1 = dirs::home_dir().expect("ホームディレクトリの取得に失敗しました");
@@ -16,8 +17,26 @@ fn main() {
 	dst_path2 = dirs::home_dir().unwrap();
 	dst_path2.push("Documents/JoinFS/whazzup.txt");
 	loop {
-		fs::copy(&src_path, &dst_path1).unwrap();
-		fs::copy(&src_path, &dst_path2).unwrap();
+		let result = fs::copy(&src_path, &dst_path1);
+		let _result = match result {
+			Ok(count) => count,
+			Err(_error) => {
+				try_count += 1;
+				println!("failed to copy file. but we are continue. {}", try_count);
+				std::thread::sleep(sleep_time);
+				continue;
+			},
+		};
+		let result = fs::copy(&src_path, &dst_path2);
+		let _result = match result {
+			Ok(count) => count,
+			Err(_error) => {
+				try_count += 1;
+				println!("failed to copy file. but we are continue. {}", try_count);
+				std::thread::sleep(sleep_time);
+				continue;
+			},
+		};
 		std::thread::sleep(sleep_time);
 	}
 }
